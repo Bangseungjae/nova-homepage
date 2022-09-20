4cc1e3a2be6814bf736376f4a6a982d7373bee91
Bangseungjae <tmdwo5825@naver.com>
2022-09-20 09:35:55 +0900

    ""

---
+++ src/main/java/nova/novahomepage/controller/HomeController.java
@@ -0,0 +1,13 @@
+package nova.novahomepage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String healthy() {
+        return "hi~";
+    }
+}
